26e66b6b68d7ce176c9289beb7b3ff29a800d63b
Martin Lindberg <30564420+fartinmartin@users.noreply.github.com>
2023-10-22 18:11:19 -0700

    ha! actually, this time is for real 🤠

--- src/lib/utils/require.ts
+++ src/lib/utils/require.ts
@@ -2,6 +2,6 @@ import * as path from "path";
 import * as fs from "fs-extra";
 
 export const injectRequire = fs.readFileSync(
-  path.join(__dirname, "./lib/require-js.js"),
+  path.resolve(__dirname, "../../external/require-js.js"),
   { encoding: "utf-8" }
 );
